cbc3afecef97533c6f7e2a37957d9a6de0bae179
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-10 19:49:59 -0300

    primeiro commit

---
+++ sistema_login/src/test/java/com/login/sistema_login/SistemaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.sistema_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
