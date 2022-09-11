e7ac1365a26683f03872813930c01d50d5408c8f
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-11 19:43:36 -0300

    Criação método Post

--- sistema_login/src/main/java/com/login/sistema_login/controllers/PessoaController.java
+++ sistema_login/src/main/java/com/login/sistema_login/controllers/PessoaController.java
@@ -1,5 +1,38 @@
 package com.login.sistema_login.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.login.sistema_login.dtos.PessoaDto;
+import com.login.sistema_login.models.PessoaModel;
+import com.login.sistema_login.services.PessoaService;
+
+
+@Controller
+@RequestMapping(value = "/usuario")
 public class PessoaController {
-    
+    @Autowired
+    private PessoaService pessoaService;
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(){
+        return "usuarios";
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<PessoaModel> cadastro(PessoaDto pessoaDto){
+        var pessoaModel = new PessoaModel();
+        BeanUtils.copyProperties(pessoaDto, pessoaModel);
+        pessoaModel.setHorarioCadastro(LocalDateTime.now(ZoneId.of("UTC-3")));
+        return new ResponseEntity<>(pessoaService.create(pessoaModel), HttpStatus.CREATED);
+    }
 }
