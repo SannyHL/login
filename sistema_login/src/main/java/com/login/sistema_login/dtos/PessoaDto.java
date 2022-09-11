d4200e5082a3c44251e55c4d7d5faa03911475d5
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-11 19:23:14 -0300

    Criação de entidade e seus atributos

--- sistema_login/src/main/java/com/login/sistema_login/dtos/PessoaDto.java
+++ sistema_login/src/main/java/com/login/sistema_login/dtos/PessoaDto.java
@@ -1,5 +1,18 @@
 package com.login.sistema_login.dtos;
 
+import javax.validation.constraints.NotBlank;
+
 public class PessoaDto {
+
+    @NotBlank
+    private Long cpf;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String senha;
+    @NotBlank
+    private String confirmaSenha;
     
 }
--- sistema_login/src/main/java/com/login/sistema_login/models/PessoaModel.java
+++ sistema_login/src/main/java/com/login/sistema_login/models/PessoaModel.java
@@ -1,5 +1,32 @@
 package com.login.sistema_login.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    @CPF
+    private String cpf;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    @Email
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+    @Column (nullable = false)
+    private String confirmaSenha;
+
+    
     
 }
