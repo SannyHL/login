3f45686144b0952bd7cf20268187d224fdcaa881
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-11 19:45:16 -0300

    criação método create

--- sistema_login/src/main/java/com/login/sistema_login/services/PessoaService.java
+++ sistema_login/src/main/java/com/login/sistema_login/services/PessoaService.java
@@ -1,5 +1,20 @@
 package com.login.sistema_login.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.login.sistema_login.models.PessoaModel;
+import com.login.sistema_login.repositories.PessoaRepository;
+
+@Service
 public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public PessoaModel create(PessoaModel pessoaModel) {
+        return pessoaRepository.save(pessoaModel);
+    }
     
+
 }
