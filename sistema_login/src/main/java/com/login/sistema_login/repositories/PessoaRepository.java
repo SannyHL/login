2d07e5b3d153428120287ae2117298184f7fac27
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-11 19:25:34 -0300

    extendendo JpaRepository

--- sistema_login/src/main/java/com/login/sistema_login/repositories/PessoaRepository.java
+++ sistema_login/src/main/java/com/login/sistema_login/repositories/PessoaRepository.java
@@ -1,5 +1,12 @@
 package com.login.sistema_login.repositories;
 
-public class PessoaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.login.sistema_login.models.PessoaModel;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<PessoaModel, String>{
+    
     
 }
