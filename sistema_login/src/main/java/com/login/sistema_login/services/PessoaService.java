d792a916da968e6ca995ecfb60d43a77fa3e001e
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-12 13:05:47 -0300

    criação de métodos

--- sistema_login/src/main/java/com/login/sistema_login/services/PessoaService.java
+++ sistema_login/src/main/java/com/login/sistema_login/services/PessoaService.java
@@ -1,5 +1,10 @@
 package com.login.sistema_login.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +17,22 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @Transactional
     public PessoaModel create(PessoaModel pessoaModel) {
         return pessoaRepository.save(pessoaModel);
     }
     
+    public List<PessoaModel> findAll(){
+        return pessoaRepository.findAll();
+    }
+
+    public Optional<PessoaModel> getCpf(String cpf) {
+        return pessoaRepository.findById(cpf);
+    }
+
+    @Transactional
+    public void delete(PessoaModel pessoaModel) {
+        pessoaRepository.delete(pessoaModel);
+    }
 
 }
