ddb470e4af3eed07f4585721f70236d60d7987f9
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-12 15:50:06 -0300

    adicionando data Lombok

--- sistema_login/src/main/java/com/login/sistema_login/dtos/PessoaDto.java
+++ sistema_login/src/main/java/com/login/sistema_login/dtos/PessoaDto.java
@@ -2,14 +2,17 @@ package com.login.sistema_login.dtos;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
+
+@Data
 public class PessoaDto {
 
     @NotBlank
-    private Long cpf;
+    private String cpf;
     @NotBlank
     private String nome;
     @NotBlank
-    private String email;
+    private String emailUsuario;
     @NotBlank
     private String senha;
     @NotBlank
