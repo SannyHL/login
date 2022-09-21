94e6f0ad0256e57ffc730feb9623143d0a4d8b05
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-21 16:57:39 -0300

    criação de query

--- sistema_login/src/main/java/com/login/sistema_login/repositories/PessoaRepository.java
+++ sistema_login/src/main/java/com/login/sistema_login/repositories/PessoaRepository.java
@@ -1,6 +1,7 @@
 package com.login.sistema_login.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.login.sistema_login.models.PessoaModel;
@@ -8,5 +9,7 @@ import com.login.sistema_login.models.PessoaModel;
 @Repository
 public interface PessoaRepository extends JpaRepository<PessoaModel, String>{
     
+    @Query(value="select * from usuarios where emailUsuario = :emailUsuario and senha = :senha", nativeQuery=true)
+    public PessoaModel Login(String emailUsuario, String senha);
     
 }
